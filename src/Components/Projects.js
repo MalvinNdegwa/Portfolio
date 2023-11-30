b99228ad81650fd80486c8ccdbc287ca46c1a3a2
Malvin Ndegwa <malvinmuthee4@gmail.com>
2023-11-30 16:05:39 +0300

    add project section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,10 +2,23 @@ export default function Projects(){
     return (
         <div className="h-screen">
             <h1 className="text-center text-3xl font-bold mb-8">MY PROJECTS</h1>
-            <p>
+            <p className="text-center text-2xl font-bold mb-8">
                 Here are some samples of my work
             </p>
-
+            <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                    <div>
+                        <img />
+                    </div>
+                    <div>
+                        <h2>Farmers seed Distribution system</h2>
+                        <p>
+                            Officia nulla elit ullamco aliqua aute tempor consequat ullamco deserunt duis non velit commodo in. Laboris non nisi sint ad ea tempor. Veniam adipisicing commodo cupidatat incididunt minim deserunt eiusmod anim ad sit. Mollit enim reprehenderit culpa elit ex. Commodo cupidatat esse dolore in cupidatat. Anim irure labore irure excepteur laborum nostrud duis nostrud eu sunt eu id. Magna eiusmod exercitation elit ut.
+                        </p>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

111928ce59ad3cd828826254fce84bbefa2933c9
Malvin Ndegwa <malvinmuthee4@gmail.com>
2023-12-05 17:31:13 +0300

    edited contact and footer section

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import '../index.css'
 export default function Header() {
     return (
-        <div className="Navbar bg-amber-300	" >
+        <div className="Navbar bg-amber-300	fixed top-0 w-screen" >
             <ul className='flex justify-between py-5 px-2'>
                 <li className='mr-auto'>Malvin Ndegwa</li>
                 <li className=' mr-4'>Home</li>
--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,33 @@
-export default function Contact(){
-    return(
-        <div className="h-screen">
-            <p>Hello</p>
+import React from 'react';
+
+const Contact = () => {
+    const emailAddress = 'Malvinmuthee4@gmail.com';
+
+    return (
+        <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+            <div className="text-center p-4 border border-gray-300 rounded-md mt-4">
+                <h2 className="text-2xl font-bold mb-2">Contact</h2>
+                <p>Wanna Talk? Don't hesitate to reach out!</p>
+                <a href={`mailto:${emailAddress}`} className="text-blue-500 font-bold hover:underline">{emailAddress}</a>
+            </div>
+            <div className="mt-8 text-center text-gray-600">
+                <p>&copy; 2023 All rights reserved</p>
+                {/* Add your social links here */}
+                {/* Example: */}
+                <div className="flex justify-center mt-2">
+                    <a href="#" className="text-gray-600 hover:text-blue-500 mx-2">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href="#" className="text-gray-600 hover:text-blue-500 mx-2">
+                        <i className="fab fa-facebook"></i>
+                    </a>
+                    <a href="#" className="text-gray-600 hover:text-blue-500 mx-2">
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
+
+export default Contact;

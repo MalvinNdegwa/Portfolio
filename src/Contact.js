5c975a602ad028d1232a4e8cbf05dd1a3dfc6a1e
Malvin Ndegwa <malvinmuthee4@gmail.com>
2023-12-20 16:16:17 +0300

    add bg colour to contact section

--- src/Contact.js
+++ src/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
 
     return (
         <div className="">
-            <div className="flex flex-col items-center justify-between p-4 border border-gray-300 rounded-md mt-4 bg-gray-100 ">
+            <div className="flex flex-col items-center justify-between p-4  bg-gray-100 bg-gradient-to-b from-intro to-transparent">
                 <div className="text-center">
                     <h2 className="text-2xl font-bold mb-2">Contact</h2>
                     <p>Wanna Talk? Don't hesitate to reach out!</p>

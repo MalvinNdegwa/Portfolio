016817c676dd2c2b8a7a94ca29403fd88eee4464
Malvin Ndegwa <malvinmuthee4@gmail.com>
2024-01-02 13:57:36 +0300

    modified logos and change text colour

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,18 +6,19 @@ import tailwind from './Images/tailwind.svg';
 import mongo from './Images/mongodb.svg';
 import mysql from './Images/mysql.svg';
 import php from './Images/php.svg';
+import nodejs from './Images/nodejs.svg';
+import Python from './Images/python.svg';
+import WordPress from './Images/wordpress.svg';
 
 export default function About() {
     return (
-        <div className="p-8  bg-intro text-white">
-            <h1 className="text-center text-3xl font-bold mb-8"> About me </h1>
+        <div className="p-8  bg-intro   ">
+            <h1 className="text-center text-3xl font-bold mb-8 text-white"> About me </h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                 <div className="justify-self-start">
                     <h2 className="text-2xl mb-4">Get To Know Me</h2>
                     <p className="text-justify">
-                        I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.
-                        I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin where I post useful content related to Web Development and Programming
-                        I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.
+                        As a junior software engineer, I possess a diverse skill set that spans both frontend and backend development. My expertise includes HTML, CSS, JavaScript, ReactJS, NodeJS, Tailwind, Python, PHP, SQL, MongoDB, and WordPress. I have cultivated these skills through a combination of coursework, internships, and personal projects. Known for my quick learning abilities and adaptability, I excel in navigating new work environments and embracing emerging technologies. My collaborative spirit thrives in both remote and on-site settings, where I contribute to the seamless development of outstanding applications.
                     </p>
                 </div>
                 <div className="text-center">
@@ -35,6 +36,12 @@ export default function About() {
                             <img src={mongo} alt="mongo_db logo" className="w-20 h-20 ml-5" />
                             <img src={mysql} alt="mysql logo" className="w-20 h-20 ml-5" />
                             <img src={php} alt="php logo" className="w-20 h-20 ml-5" />
+                        </div>
+                        <div className="flex mt-4">
+                            <img src={nodejs} alt="nodejs logo" className="w-20 h-20 mr-5" />
+                            <img src={Python} alt="python logo" className="w-20 h-20 ml-5" />
+                            <img src={WordPress} alt="wordpress logo" className="w-20 h-20 ml-5" />
+                            
                             {/* Add more skills or adjust the styling as needed */}
                         </div>
                     </div>
